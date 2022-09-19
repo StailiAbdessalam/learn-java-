a8caa51526fd9b9c2a09490ef80f64cfe1de1030
abdessalm staili <abdoycode@gmail.com>
2022-09-19 15:11:03 +0100

    final version of plindrome game number

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,17 @@
+public class PalindromeNumber {
+    public static  void main(String args[]){
+        int numberChecked = 5545;
+
+        int number=numberChecked,r,dev=0;
+        while(numberChecked>0){
+            r=(numberChecked)%10;
+            numberChecked=numberChecked/10;
+            dev=(dev*10)+r;
+        }
+        if(dev==number)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+
+    }
+}
