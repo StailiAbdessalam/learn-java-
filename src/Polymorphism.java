10bf28fb3cd981635b856eff01713fccda0a1c31
abdessalm staili <abdoycode@gmail.com>
2022-09-20 11:31:01 +0100

    Polymorphim class to test exemle

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -1,9 +1,31 @@
-public class Polymorphism {
+public class Polymorphism extends Aou {
+    int number;
     public static void main (String arg[]){
         System.out.println("is just checked concept polymorphism");
         // we have two type of polymorphism first is compile-time Polymorphism and second is runtime Polymorphism
         // polymorphism is derived from 2 greek words : poly means many and morphs means forms
+        // upcasting and downcasting
+            }
+    public void  test (int number){
+        System.out.println("hiohdiof");
+        System.out.println(a);
+        Aou test =  new Polymorphism();
+        number = test.a;
+        System.out.println("hhhhhhhhhhhhhhhhhhhhh");
+        System.out.println(number);
+
 
     }
+
+
+
+    public void test(){
+        int t=1;
+        System.out.println(t);
+
+    }
+}
+class Aou{
+    int a=0;
 }
 
