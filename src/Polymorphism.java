127e5063130d35a252c8b7bcbebfa8c9186fc9a4
abdessalm staili <abdoycode@gmail.com>
2022-09-20 09:39:46 +0100

    initialation the class to learn pomymorphism with exemple

---
+++ src/Polymorphism.java
@@ -0,0 +1,9 @@
+public class Polymorphism {
+    public static void main (String arg[]){
+        System.out.println("is just checked concept polymorphism");
+        // we have two type of polymorphism first is compile-time Polymorphism and second is runtime Polymorphism
+        // polymorphism is derived from 2 greek words : poly means many and morphs means forms
+
+    }
+}
+
